9c4927ca8e181d4116b36046900d53284cdf78da
J. Natalia Ramirez <jnataliaramirez@gmail.com>
2021-08-12 21:14:21 +0200

    Add menu collapsable for fill and share, work in file collapsable.js

--- src/js/00_collapsable.js
+++ src/js/00_collapsable.js
@@ -1,14 +1,53 @@
-//collapsable titulos profile cards
 'use strict';
 
-const collapsableButton = document.querySelector('.js_collapsable');
+// * collapsable titulos profile cards
 
-const collapsableCard = document.querySelector('.js_collapsable_design');
+// ! section design
+const collapsableButtonDesign = document.querySelector('.js_collapsable_design_button');
+const collapsableCardDesign = document.querySelector('.js_collapsable_design');
 
-function handleCollapsableClick () {
-  collapsableCard.classList.toggle('hidden');
+function handleCollapsableDesign () {
+  collapsableCardDesign.classList.toggle('hidden');
+  collapsableCardFill.classList.add('hidden');
+  collapsableCardShare.classList.add('hidden');
+
 }
 
+collapsableButtonDesign.addEventListener('click',handleCollapsableDesign);
+
+// ! section fill
+const collapsableButtonFill = document.querySelector('.js_collapsable_fill_button');
+const collapsableCardFill = document.querySelector('.js_collapsable_fill');
+
+function handleCollapsableFill () {
+  collapsableCardFill.classList.toggle('hidden');
+  collapsableCardDesign.classList.add('hidden');
+  collapsableCardShare.classList.add('hidden');
+}
+
+collapsableButtonFill.addEventListener('click',handleCollapsableFill);
+
+// ! section share
+const collapsableButtonShare = document.querySelector('.js_collapsable_share_button');
+const collapsableCardShare = document.querySelector('.js_collapsable_share');
+
+function handleCollapsableShare () {
+  collapsableCardShare.classList.toggle('hidden');
+  collapsableCardDesign.classList.add('hidden');
+  collapsableCardFill.classList.add('hidden');
+}
+
+collapsableButtonShare.addEventListener('click',handleCollapsableShare);
+
+// const collapsableButton = document.querySelector('.js_collapsable');
+
+// const collapsableCard = document.querySelector('.js_collapsable_design');
+
+// function handleCollapsableClick () {
+//   collapsableCard.classList.toggle('hidden');
+  
+// }
 
 
-collapsableButton.addEventListener('click',handleCollapsableClick);
+
+// collapsableButton.addEventListener('click',handleCollapsableClick);
