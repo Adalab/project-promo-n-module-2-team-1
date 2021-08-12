20ed6fd1e9db12b049b2046de54b77d391e3959a
iratxetos <iratxetxu@gmail.com>
2021-08-12 20:24:15 +0200

    added name javascript

--- src/js/02_form.js
+++ src/js/02_form.js
@@ -1,7 +1,13 @@
 //formulario rellena profile cards
 'use strict';
 
+const formName = document.querySelector('.js_name');
+const formPreviewName = document.querySelector('.js_preview_name');
 
+function handleName(){
+    formPreviewName.innerHTML = formName.value;
+}
 
+formName.addEventListener("keyup", handleName);
 
 
