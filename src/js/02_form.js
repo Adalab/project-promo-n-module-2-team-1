51ef731c7c5a2c6ecf198b604ca533800e874363
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-09-02 14:09:59 +0200

    added form js

--- src/js/02_form.js
+++ src/js/02_form.js
@@ -1,44 +1,55 @@
 //formulario rellena profile cards
 'use strict';
 
-const formName = document.querySelector('.js_name');
+//preview card: escribimos las constantes solo del preview
+const palletteOne =document.querySelector('.js_');
+const palletteTwo=document.querySelector('.js_');
+const palleteThree=document.querySelector('.js_');
 const formPreviewName = document.querySelector('.js_preview_name');
-
-function handleName(){
-formPreviewName.innerHTML = formName.value;
-}
-
-formName.addEventListener("keyup", handleName);
-
-
-//RRSS
-
-//inputs form
-const formPhone = document.querySelector('.js_phone');
-const formEmail =document.querySelector('.js_email');
-const formGithub = document.querySelector('.js_github');
-const formLinkedin =document.querySelector('.js_linkedin');
-
-//preview card
-
 const previewLinkedin = document.querySelector('.js_preview_linkedin');
 const previewPhone = document.querySelector('.js_preview_phone');
 const previewEmail = document.querySelector('.js_preview_email');
 const previewGithub = document.querySelector('.js_preview_github');
+const formPreviewJob = document.querySelector('.js_preview_job');
 
+//hacemos el objeto que comprende todo el formulario: diseña y rellena
+const formObject = {
+    palette: "",
+    name: "",
+    job: "",
+    phone: "",
+    email: "",
+    linkedin: "",
+    github: "",
+    photo: ""
+  }
 
+  
+//seleccionamos todo el formulario para aplicarle el evento
 
-function handledUpdateGithub(){
-    
-   previewGithub.setAttribute("href" , formGithub.value);
+const formEl = document.querySelector('.js_form');
 
+//creamos la funcion para escribir en la previsualización el valor de cada uno de los inputs del objeto
+function handledFormElUpdate (event){
+    //nos devuelve el ID del input
+    const inputId = event.target.id;
+    //nos devuelve el valor del input
+    const inputValue = event.target.value;
+//asigna al ID de cada objeto un valor
+//el ID del objeto y el ID del formulario tienen que coincidir
+ formObject[inputId]=inputValue;
+//escribe en cada elemento html de la card el valor del input del objeto
+   formPreviewName.innerHTML = formObject.name;
+
+   formPreviewJob.innerHTML=formObject.job;
+   previewLinkedin.href = `https://linkedin.com/${formObject.linkedin}`;
+
+   
 }
+//el evento sobre el formulario completo
+formEl.addEventListener('change' , handledFormElUpdate );
 
-function setValueInput (){
-    previewGithub.setAttribute("href" , formGithub.value);
-    
-}
+//importante saber cual es el target y el currentTarget de la función, en este caso el target=cada elemento del formulario que tiene el foco puesto y el currentTarget=el formulario entero
 
-formGithub.addEventListener('change' , setValueInput());
 
 
