0ed9af654051cadbcb3cc02282d82b5b2cc4b9a0
iratxetos <iratxetxu@gmail.com>
2021-09-07 15:27:44 +0200

    added reset

--- src/js/03_reset.js
+++ src/js/03_reset.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// formEl = document.querySelector('.js_form');
 const btnReset = document.querySelector('.js-preview__button--reset');
 
-function resetValue(ev) {
-    btnReset.reset();
-    return resetValue;
+function resetValue(event) {
+
+    formEl.reset();
+    event.preventDefault();
+
 }
 
 btnReset.addEventListener('click', resetValue);
