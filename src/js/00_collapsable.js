cd4580c0d2353b8c10359901e0d4e9bb99e0e00b
iratxetos <iratxetxu@gmail.com>
2021-09-14 23:38:39 +0200

    added arrows

--- src/js/00_collapsable.js
+++ src/js/00_collapsable.js
@@ -1,53 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable indent */
 'use strict';
 
-// * collapsable titulos profile cards
+const headerClicks = document.querySelectorAll('.js-collapsible');
 
-// ! section design
-const collapsableButtonDesign = document.querySelector('.js_collapsable_design_button');
-const collapsableCardDesign = document.querySelector('.js_collapsable_design');
-const collapsableArrowDesign = document.querySelector('.js_arrow_design');
-
-function handleCollapsableDesign() {
-  collapsableCardDesign.classList.toggle('hidden');
-  collapsableArrowDesign.classList.toggle('fa-chevron-up');
-  collapsableCardFill.classList.add('hidden');
-  collapsableCardShare.classList.add('hidden');
-
-
+function handleHeaderClick(e) {
+  const t = e.currentTarget.closest('.js-hidden')
+    , n = document.querySelectorAll('.js-hidden');
+  for (const e of n) { t === e ? e.classList.toggle('hidden') : e.classList.add('hidden'); }
 }
-
-collapsableButtonDesign.addEventListener('click', handleCollapsableDesign);
-
-// ! section fill
-const collapsableButtonFill = document.querySelector('.js_collapsable_fill_button');
-const collapsableCardFill = document.querySelector('.js_collapsable_fill');
-const collapsableArrowFill = document.querySelector('.js_arrow_fill');
-
-
-function handleCollapsableFill() {
-  collapsableCardFill.classList.toggle('hidden');
-  collapsableArrowFill.classList.toggle('fa-chevron-up');
-  collapsableCardDesign.classList.add('hidden');
-  collapsableCardShare.classList.add('hidden');
-
-}
-
-collapsableButtonFill.addEventListener('click', handleCollapsableFill);
-
-// ! section share
-const collapsableButtonShare = document.querySelector('.js_collapsable_share_button');
-const collapsableCardShare = document.querySelector('.js_collapsable_share');
-const collapsableArrowShare = document.querySelector('.js_arrow_share');
-
-
-function handleCollapsableShare() {
-  collapsableCardShare.classList.toggle('hidden');
-  collapsableArrowShare.classList.toggle('fa-chevron-up');
-  collapsableCardDesign.classList.add('hidden');
-  collapsableCardFill.classList.add('hidden');
-
-}
-
-collapsableButtonShare.addEventListener('click', handleCollapsableShare);
-
+for (const e of headerClicks) { e.addEventListener('click', handleHeaderClick); }
 
