df39af83316dcd05107d484eb199df6cc9d52a6a
iratxetos <iratxetxu@gmail.com>
2021-09-16 00:22:55 +0200

    added changes in collapsable

--- src/js/00_collapsable.js
+++ src/js/00_collapsable.js
@@ -1,13 +1,12 @@
-/* eslint-disable no-console */
-/* eslint-disable indent */
 'use strict';
 
-// const headerClicks = document.querySelectorAll('.js-collapsible');
+const headerClicks = document.querySelectorAll('.js-collapsible');
 
-// function handleHeaderClick(e) {
-//   const t = e.currentTarget.closest('.js-hidden')
-//     , n = document.querySelectorAll('.js-hidden');
-//   for (const e of n) { t === e ? e.classList.toggle('hidden') : e.classList.add('hidden'); }
-// }
-// for (const e of headerClicks) { e.addEventListener('click', handleHeaderClick); }
+function handleHeaderClick(e) {
+    const t = e.currentTarget.closest('.js-hidden'),
+        n = document.querySelectorAll('.js-hidden');
+    for (const e of n) { t === e ? e.classList.toggle('hidden') : e.classList.add('hidden'); }
+}
+for (const e of headerClicks) { e.addEventListener('click', handleHeaderClick); }
+
 
